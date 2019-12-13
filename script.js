36f15b5152c149a319161221cd36f56ea68e7e37
Craig Clark <crgcrck7@gmail.com>
2019-12-13 02:24:42 -0500

    password works in console

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ var promptLength = prompt("How many characters pick between 8-128");
 
 //objects//
 
-const resultEl = document.getElementById('result');
+const resultEl = document.getElementById("#result");
 const upperEl = document.getElementById('uppercase');
 const lowerEl = document.getElementById('lowercase');
 const numberEl = document.getElementById('numbersEl');
@@ -18,14 +18,64 @@ const symbolEl = document.getElementById('symbol');
 var generate = document.querySelector("#generate");
 const clipboardEl = document.getElementById('clipboard');
 
+const randomFunc = {
+lower: getRandomLower,
+upper: getRandomUpper,
+number: getRandomNumber,
+symbol: getRandomSymbol
+
+};
+
+//Generate Events
 
 generate.addEventListener("click", function(event) {
+    var hasLower = confirmLower;
+    var hasNumber = confirmNumber;
+    var hasSymbol = confirmSymbol;
+    var hasUpper = confirmUpper;
+    var hasLength = promptLength;
     event.preventDefault();
 
-console.log(event);
+resultEl.innerText = generatePassword(
+    hasLower, 
+    hasUpper, 
+    hasNumber, 
+    hasSymbol, 
+    hasLength
+    );
+});
 
-//
 
+// Generate Password Func//
+function generatePassword(lower,upper,number,symbol,length) {
+
+    let generatePassword ="";
+
+    const typesCount = lower + upper + number +symbol;
+
+    //Filter//
+    
+    const typesArr = [{lower}, {upper}, {number}, {symbol}].filter
+    (
+        item =>Object.values(item)[0]
+    );
+   
+    console.log("typesArr: ", typesArr);
+
+    if(typesCount ===0) {
+        return"";
+ }
+
+for(let i = 0; i < length; i+= typesCount ) {
+  typesArr.forEach(type=> {
+    const funcName = Object.keys(type)[0];
+
+    generatePassword += randomFunc[funcName]();
+  });
+}
+
+console.log(generatePassword);
+}
 
 // I need a for loop t the number of times inputed for length, using the true reponses//
 
@@ -56,4 +106,4 @@ function getRandomUpper() {
 // Have to run function to get random passowrd//
 
 // have to print out out put//
-});
+
